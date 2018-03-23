c593fe6142ea3b6641cee272a9b570153a1b9fcc
Marcus Christiansen <marcus@firingsquad.co.za>
2018-03-23 09:43:29 +0200

    add createMessage and newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to server");
-
-  socket.emit('createMessage', {
-      from: 'Marcus',
-      text: 'Howsit mense'
-  })
 });
 
 socket.on("disconnect", function() {
