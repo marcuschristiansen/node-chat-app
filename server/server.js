c22e1acb071a6fe34de19b5c86698112ba2af270
Marcus Christiansen <marcus@firingsquad.co.za>
2018-03-27 14:59:12 +0200

    add user verification

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const socketIO = require('socket.io')
 
 const { generateMessage, generateLocationMessage } = require('./utils/message')
+const { isRealString } = require('./utils/validation')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
@@ -16,6 +17,14 @@ io.on('connection', (socket) => {
     console.log('New user connected.')
 
     socket.emit("newMessage", generateMessage('Admin', 'Welcome to the chat app.'));
+
+    socket.on('join', (params, callback) => {
+        if(!isRealString(params.name) || !isRealString(params.room)) {
+            callback('Name and room name are required');
+        }
+
+        callback();
+    })
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'))
 
