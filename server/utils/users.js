75b6b25e82eb47c3e18a9a2710cf6a4b5f6dc711
Marcus Christiansen <marcus@firingsquad.co.za>
2018-03-30 11:34:25 +0200

    Final

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,38 +1,55 @@
+[{
+  id: '/#12poiajdspfoif',
+  name: 'Andrew',
+  room: 'The Office Fans'
+}]
+
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
 class Users {
-    constructor () {
-        this.users = []
-    }
-    addUser (id, name, room) {
-        var user = {id, name, room}
-        this.users.push(user);
-        return user;
-    }
-    removeUser (id) {
-        var user = this.getUser(id)
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
 
-        if(user) {
-            this.users = this.users.filter((user) => {
-                user.id !== id
-            })
-        }
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
 
-        return user;
-    }
-    getUser (id) {
-        return this.users.filter((user) => {
-            return user.id === id
-        })[0]
-    }
-    getUserList (room) {
-        var users = this.users.filter((user) => {
-            return user.room === room
-        })
-        var namesArray = users.map((user) => {
-            return user.name
-        })
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
 
-        return namesArray
-    }
+    return namesArray;
+  }
 }
 
-module.exports = { Users }
+module.exports = {Users};
+
+ // class Person {
+ //   constructor (name, age) {
+ //     this.name = name;
+ //     this.age = age;
+ //   }
+ //   getUserDescription () {
+ //     return `${this.name} is ${this.age} year(s) old.`;
+ //   }
+ // }
+ //
+ // var me = new Person('Andrew', 25);
+ // var description = me.getUserDescription();
+ // console.log(description);
